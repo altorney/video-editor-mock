dd14c84deee67a090571d56a4bbb41b4a6d56330
altorney <altorney@users.noreply.github.com>
2023-07-08 21:41:28 +0000

    fixed video spacer resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import VideoSpacer from "./videoSpacer";
 const dataInital = [
   { id: 1, name: "John1" },
   { id: 2, name: "Jane" },
   { id: 3, name: "Bob" },
+  { id: 3, name: "Bob2" },
+  { id: 4, name: "Bob2" },
 ];
 interface Item {
   id: number;
@@ -13,15 +16,13 @@ interface Item {
 
 const App = () => {
   const [selected, setSelected] = useState<number | null>(null);
-  console.log("rendered");
   const [data, setData] = useState<Array<Item>>(dataInital);
 
   const handleDelete = (index: number) => {
     const newData = data.filter((item, i) => i !== index);
     setData(newData);
   };
-
-  useEffect(() => {
+ useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Delete" && selected !== null) {
         handleDelete(selected);
@@ -34,11 +35,9 @@ const App = () => {
     return () => {
       document.body.removeEventListener("keydown", handleKeyDown);
     };
-  }, [handleDelete]);
-
-  const items = data.map((item, index) => (
+  }, [handleDelete]);  const items = data.map((item, index) => (
     <>
-      <span className="video-spacer" key={item.id + "spacer"}></span>
+      <VideoSpacer key={item.id + "spacer"} id={item.id + "spacer"} />
       <div
         className={index === selected ? "video selected" : "video"}
         key={item.id}
